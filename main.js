eb396577c28221481d0a1fb5ffba436f4ced70c1
AMAN SINGH BISHT <amansinghbisht6@gmail.com>
2024-11-04 12:20:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
-        alert("you must write something!")
+        alert("you must write something!") //Checks if the input field is empty. If it is, it alerts the user.
     }
     else{
         let li = document.createElement("li");
@@ -11,11 +11,11 @@ function addTask(){
         listContainer.appendChild(li)
 
         let span = document.createElement("span")
-        span.innerHTML = "\u00d7";
+        span.innerHTML = "\u00d7"; //A span element containing × is added to each li. This span acts as a delete button.
         li.appendChild(span);
     }
     inputBox.value = '';
-    savedata();
+    savedata(); //Save to Local Storage
 }
 
 listContainer.addEventListener("click", function(e){
@@ -24,17 +24,21 @@ listContainer.addEventListener("click", function(e){
         savedata();
     }
     else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove()
-        savedata();
+        e.target.parentElement.remove() //Delete Task
+        savedata(); //Save to Local Storage
     }
 }, false)
 
-function savedata(){
+function savedata() //Save Data Function:
+{
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showtask(){
+function showtask() 
+//When the page loads, showtask() retrieves the saved list from localStorage and sets listContainer's HTML to that saved data, restoring the list.
+{
     listContainer.innerHTML = localStorage.getItem("data")
 }
 
 showtask();
+//Calls showtask() to ensure that any previously saved tasks are displayed when the page loads.
